f785ac51ccde77655b7de045548439f589fd78c4
Chibueze Nwaka <Chibueze.Nwaka@nnpcgroup.com>
2023-11-05 22:40:58 +0100

    add fastapi

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uvicorn
 from bot import process_message
 
 class Message(BaseModel):
@@ -14,3 +15,7 @@ async def chatbot(msg: Message):
     message = process_message(msg.msg)
     return message
 
+
+if __name__ == "__main__": 
+    # uvicorn main:app --reload
+    uvicorn.run("server.api:app", host="0.0.0.0", port=8000, reload=True)
