831c657363b696844edf3702c70d859663bfd553
Chibueze Nwaka <Chibueze.Nwaka@nnpcgroup.com>
2023-11-05 23:54:28 +0100

    edited requirements.txt

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uvicorn
+
 from bot import process_message
 
 class Message(BaseModel):
     msg: str
 
-
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/chatbot/")
 async def chatbot(msg: Message):
     message = process_message(msg.msg)
     return message
-
-
-# if __name__ == "__main__": 
-#     # uvicorn main:app --reload
-#     uvicorn.run("server.api:app", host="0.0.0.0", port=8000, reload=True)
